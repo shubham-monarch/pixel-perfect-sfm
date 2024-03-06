d4a71983a6d1f303902afad4c1724e20d5939059
Shubham Kumar <skumar@monarchtractor.com>
2024-03-06 14:43:28 +0530

    fixed dz issue + udpated rig_ba.sh

--- rig_ba.sh
+++ rig_ba.sh
@@ -4,4 +4,12 @@ COLMAP_EXE_PATH=/usr/local/bin
 INPUT_PATH=$HOME/pixel-perfect-sfm/outputs/monarch-demo/ref_locked/
 OUTPUT_PATH=$PWD
 
-$COLMAP_EXE_PATH/colmap rig_bundle_adjuster --input_path $INPUT_PATH --output_path $OUTPUT_PATH --rig_config_path rig.json --BundleAdjustment.refine_focal_length 0 --BundleAdjustment.refine_extra_params 0 --BundleAdjustment.refine_extrinsics 0 
+$COLMAP_EXE_PATH/colmap rig_bundle_adjuster \
+	--input_path $INPUT_PATH \
+	--output_path $OUTPUT_PATH \
+	--rig_config_path rig.json \
+	--estimate_rig_relative_poses False\ 
+	--BundleAdjustment.refine_focal_length 0 \
+	--BundleAdjustment.refine_principal_point 0\
+	--BundleAdjustment.refine_extra_params 1 \
+	--BundleAdjustment.refine_extrinsics 1  
